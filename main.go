e1563487a5a0f2625fff26fcf6f235614624669c
Erik Daughtrey <ed151@duke.edu>
2023-01-16 08:04:20 -0500

    added main

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+// vault "github.com/OIT-ADS/vault-go/vault"
+
+import (
+	"fmt"
+
+	"github.com/OIT-ADS/vault-go/vault_go"
+)
+
+func main() {
+	fmt.Printf("Vault main\n")
+	vault_go.Vault()
+
+}
